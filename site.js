78c6da915aaa7a04b37ad958d79101f316916a99
Devon <Devonhelmrath@gmail.com>
2025-02-17 15:02:44 -0600

    Added Carousel1

--- site.js
+++ site.js
@@ -1,5 +1,16 @@
 console.log('eqwoigbqeiuogbqeoigbqegoiqebgqeoiugbweroigbwargiujwsrabgliwarujgb')
 
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+const prev = document.querySelector('#prev')
+const next = document.querySelector('#next')
 
 const key = "It's a secret to everybody."
 
@@ -20,3 +31,38 @@ else if(isEvening){
 
 secretMessage = "it's a reference to the 2nd best zelda game"
 localStorage.setItem(key, secretMessage)
+
+//figured it was good practice to reset timer per click
+let currentImage = 0
+function startInterval() {
+    practice = setInterval(() => {
+    currentImage++
+    console.log('potato')
+    showImages()
+}, 5000)
+}
+function resetInterval() {
+clearInterval(practice)
+startInterval()
+}
+startInterval()
+prev.addEventListener('click', () => {
+    currentImage--
+    showImages()
+    resetInterval()
+})
+next.addEventListener('click', () => {
+    currentImage++
+    showImages()
+    resetInterval()
+})
+
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
